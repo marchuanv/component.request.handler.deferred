a9f08d8039da9bd0a0b82e5b3cbbd5090c5a9a76
marchuanv <marchuanv@gmail.com>
2020-05-03 07:21:20 +0200

    fix

--- test.js
+++ test.js
@@ -2,18 +2,22 @@ const componentRequestHandlerDeferred = require("./component.request.handler.def
 const logging = require("logging");
 logging.config(["Request Handler Deferred", "Request Handler Route","Request Handler"]);
 (async()=>{
-    (await componentRequestHandlerDeferred.handle({ port: 3000, path: "/test" })).receive((request) => {
-        return new Promise((resolve)=>{
-            setTimeout(()=>{
-                resolve({
-                    statusCode: 200,
-                    statusMessage:"Success",
-                    data: "",
-                    contentType: "text/plain"
-                });
-            },7000);
+    const requeue = async () => {
+        (await componentRequestHandlerDeferred.handle({ port: 3000, path: "/test" })).receive((request) => {
+            requeue();
+            return new Promise((resolve)=>{
+                setTimeout(()=>{
+                    resolve({
+                        statusCode: 200,
+                        statusMessage:"Success",
+                        data: "",
+                        contentType: "text/plain"
+                    });
+                },1000);
+            });
         });
-    });
+    };
+    requeue();
 })().catch((err)=>{
     console.log(err);
 });
