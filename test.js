b9009df35f7d5b7260415899715fa7771620c7c1
marchuanv <marchuanv@gmail.com>
2020-06-03 13:02:22 +0200

    fixing tests

--- test.js
+++ test.js
@@ -1,6 +1,4 @@
 const componentRequestHandlerDeferred = require("./component.request.handler.deferred.js");
-const logging = require("logging");
-logging.config(["Request Handler Deferred", "Request Handler Route","Request Handler"]);
 (async()=>{
     const requeue = async () => {
         (await componentRequestHandlerDeferred.handle({ port: 3000, path: "/test" })).receive((request) => {
